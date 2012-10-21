3dc11581009bad97f26dfa306c3ed62bad90ecc4
Justin Erny <jerny@vailsys.com>
2012-10-21 13:58:42 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChefTest::Application.config.secret_token = '63d43c50055e41c9a67dd735c7e672312b9918ad1430004df9ce1c4f120bb5273feee22ec46fb40b0f0642935accfc3153cdce90b2d612afe2ae2aee44fd639f'
