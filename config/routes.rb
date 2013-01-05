4abb0641ed52998bb364b922339eb640996832a8
Justin Erny <jerny@vailsys.com>
2013-01-04 18:36:30 -0600

    added files to ignore on archiving

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ ChefTest::Application.routes.draw do
   match '/' =>  'explosion#index'
   match '/nancat' =>  'nancat#index'
   resources :explosion
+
+  resources :dashboard
+  resources :project
 end
